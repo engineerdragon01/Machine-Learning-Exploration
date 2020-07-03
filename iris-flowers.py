4d941b3fca5362c8e29107321eccbc62fde26193
Adam Chois <dragonboy174@gmail.com>
2020-07-02 23:52:52 -0700

    finished all data representations and selected model

--- iris-flowers.py
+++ iris-flowers.py
@@ -6,7 +6,7 @@ import pandas
 import sklearn
 
 from pandas.plotting import scatter_matrix
-import matplotlib.pyplot as plt
+from matplotlib import pyplot
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
@@ -31,3 +31,55 @@ dataset = pandas.read_csv(url, names=names)
 #print(dataset.describe())
 # groupby, size : can look at number of instances (rows) that belong to each class
 #print(dataset.groupby('class').size())
+
+# box and whisker plots
+dataset.plot(kind="box", subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+
+# histograms : two of them have a seemingly Gaussian distribution
+dataset.hist()
+pyplot.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
+
+# Split-out validation dataset
+array = dataset.values
+x = array[:,0:4]
+y = array[:,4]
+x_train, x_validation, y_train, y_validation = model_selection.train_test_split(x, y, test_size=.2)
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+# evaluate each model
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.StratifiedKFold(n_splits=10, random_state=1, shuffle = True)
+    cv_results = model_selection.cross_val_score(model, x_train, y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+# Compare Algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+
+# Make predictions on validation dataset
+model = SVC(gamma='auto') # put most accurate model here
+model.fit(x_train, y_train)
+predictions = model.predict(x_validation)
+
+# Evaluate predictions
+print(accuracy_score(y_validation, predictions))
+print(confusion_matrix(y_validation, predictions))
+print(classification_report(y_validation, predictions))
